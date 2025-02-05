64f8760065fd9b0d1119cc0c3ebbfeea9447cb8c
basena <basinamaria2@gmail.com>
2025-02-05 21:52:16 +0300

    Add files via upload

---
+++ ооп Лабораторная работа 3/task1.py
@@ -0,0 +1,62 @@
+class Book:
+    def __init__(self, name: str, author: str):
+        self._name = name  # Защищённые атрибуты
+        self._author = author
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def author(self) -> str:
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages  # Используем свойство для установки значения
+
+    @property
+    def pages(self) -> int:
+        return self._pages
+
+    @pages.setter
+    def pages(self, value: int):
+        if not isinstance(value, int):
+            raise TypeError("Количество страниц должно быть целым числом.")
+        if value <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом.")
+        self._pages = value
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self, value: float):
+        if not isinstance(value, (int, float)):
+            raise ValueError("Продолжительность должна быть числом.")
+        if value <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом.")
+        self._duration = value
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration})"
+
+
